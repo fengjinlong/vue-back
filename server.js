0f31ec5a12cd92b90fd91fa446e2af37bbde2972
fengjinlong <18618166564@163.com>
2019-11-21 21:51:15 +0800

    a

---
+++ server.js
@@ -0,0 +1,18 @@
+let http = require('http')
+let users = [
+  {id: 1, name: 'zzz1'},
+  {id: 1, name: 'zzz2'},
+  {id: 1, name: 'zzz3'}
+]
+let server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // Access-Control-Allow-Origin
+  if (req.url === '/api') {
+    res.end(JSON.stringify(users))
+  } else {
+    res.end('not found')
+  }
+})
+server.listen(3000, () => {
+  console.log('接口服务器 🍊 3000')
+})
